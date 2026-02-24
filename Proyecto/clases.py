336b585554831494da7d8804c595680f8e1a5acb
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-02-24 02:07:46 -0600

    Update clases.py

--- Proyecto/clases.py
+++ Proyecto/clases.py
@@ -1,1 +1,29 @@
-class vertices
+class vertices:
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.dist_min
+        self.visitado = False
+        self.previo = None
+
+class aristas:
+    def __init__(self, v_1, v_2, w):
+        self.vertice1 = v_1
+        self.vertice2 = v_2
+        self.peso = w
+
+class grafica:
+    def __init__(self,):
+        self.vertices = {}
+        self.aristas = []
+    
+    def agregar_vertice(self, nombre):
+        if nombre not in self.vertices:
+            self.vertices[nombre] = self.vertices(nombre)
+
+    def agregar_aristas(self, v_1, v_2, w):
+        if v_1 not in self.vertices or v_2 not in self.vertices:
+            print("No existen tales vértices, introduce vertices que sí existan en la gráfica.")
+            return
+
+    def vecinos(self, nombre)
+        return[(u.v_1; u.v_2) for u in self.aristas if u.v_1 == origen]
