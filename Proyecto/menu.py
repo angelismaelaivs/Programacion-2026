4f23c7d1c396c4f2da468a1f9d7be827f2d1813d
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-02-24 17:30:02 -0600

    Update menu.py

--- Proyecto/menu.py
+++ Proyecto/menu.py
@@ -1,27 +1,34 @@
-import clases
+from clases import grafica
+from clases import vertices
+from clases import aristas
+
 
 def __init__(self):
     self.grafica = grafica()
 
-def mostrar_menu(self): #por ahora solo funcionara 1, 2 y 0
-        print("\n--- MENÚ DIJKSTRA ---")
-        print("1. Agregar vértice")
-        print("2. Agregar arista")
-        print("3. Ejecutar Dijkstra") 
-        print("4. Mostrar camino mínimo") 
-        print("0. Salir")
-    
-        opcion = input("Opción: ")
-
-        if opcion == "1":
-            v = input("Vértice: ")
-            grafica.agregar_vertice(v)
-
-        elif opcion == "2":
-            u = input("Origen: ")
-            v = input("Destino: ")
-            peso = float(input("Peso: "))
-            grafica.agregar_arista(u, v, peso)
+def mostrar_menu(self): #por ahora solo funcionará 1, 2 y 0 ya que aun no hago el algoritmo de Dijkstra y el camino mínimo
+        while True:
+            print("\n--- Seleccione una opción ---")
+            print("1. Agregar vértice")
+            print("2. Agregar arista")
+            print("3. Ejecutar Dijkstra") 
+            print("4. Mostrar camino mínimo") 
+            print("0. Salir")
         
-        elif opcion == "0":
-            break
+            opcion = input("Opción: ")
+
+            if opcion == "1":
+                v = input("Vértice: ")
+                grafica.agregar_vertice(v)
+
+            elif opcion == "2":
+                u = input("Origen: ")
+                v = input("Destino: ")
+                peso = float(input("Peso: "))
+                grafica.agregar_arista(u, v, peso)
+            
+            elif opcion == "0":
+                break 
+
+            else:
+                print("La opción no es válida. Intente con 1, 2, 3, 4 o 0.")
