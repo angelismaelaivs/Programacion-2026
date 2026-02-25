052059ae74e956dd5d135f15a559e0a3d8e9c960
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-02-24 21:56:46 -0600

    Actualizar menu.py

--- Proyecto/menu.py
+++ Proyecto/menu.py
@@ -1,34 +1,16 @@
-from clases import grafica
-from clases import vertices
-from clases import aristas
+from vertice import Vertice
 
+def main():
+    print("=== Crear un Vértice ===")
+    
+    nombre = input("Ingresa el nombre del vértice (una letra): ")
+    peso = float(input("Ingresa el peso del vértice (número positivo): "))
+    
+    v = Vertice(nombre, peso)
+    
+    print("\nVértice creado:")
+    print("Nombre:", v.nombre)
+    print("Peso:", v.peso)
 
-def __init__(self):
-    self.grafica = grafica()
-
-def mostrar_menu(self): #por ahora solo funcionará 1, 2 y 0 ya que aun no hago el algoritmo de Dijkstra y el camino mínimo
-        while True:
-            print("\n--- Seleccione una opción ---")
-            print("1. Agregar vértice")
-            print("2. Agregar arista")
-            print("3. Ejecutar Dijkstra") 
-            print("4. Mostrar camino mínimo") 
-            print("0. Salir")
-        
-            opcion = input("Opción: ")
-
-            if opcion == "1":
-                v = input("Vértice: ")
-                grafica.agregar_vertice(v)
-
-            elif opcion == "2":
-                u = input("Origen: ")
-                v = input("Destino: ")
-                peso = float(input("Peso: "))
-                grafica.agregar_arista(u, v, peso)
-            
-            elif opcion == "0":
-                break 
-
-            else:
-                print("La opción no es válida. Intente con 1, 2, 3, 4 o 0.")
+if __name__ == "__main__":
+    main()
