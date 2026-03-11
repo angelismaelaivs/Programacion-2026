91235c2ba6d47a2374f53f82246b1f1059ada55e
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-03-11 10:12:55 -0600

    Update Menu.py

--- teoria/Menu.py
+++ teoria/Menu.py
@@ -20,27 +20,25 @@ class Menu:
         
 
   def procesaOpcion(self, opcion):
-        while True:
-            opcion = input('Elija una opción:')
-            if opcion == '1':
-                print('Usted eligió información de la cuenta.')
-                self.cuenta.informacion()
-                self.despliegaMenu()
-            elif opcion == "2":
-                print("Usted eligió hacer un deposito, ingrese la cantidad a depositar:")
-                cantidad = float(input())
-                self.cuenta.depositar(cantidad)
-                print("   Su saldo es de: ", self.cuenta.saldo, "$")
-                self.despliegaMenu()
-            elif opcion == "3":
-                print('Usted eligió hacer un retiro, ingrese la cantidad a retirar:')
-                cantidad = float(input())
-                self.cuenta.retirar(cantidad)
-                print("Su saldo es de: ", self.cuenta.saldo, "$")
-                self.despliegaMenu()
-            elif opcion == "4":
-                print("Usted eligió terminar la sesion, vuelva pronto.")
-                break
-            else:
-                print('Opcion no valida, elija entre las opciones 1, 2, 3 o 4.')
-                self.despliegaMenu()
+        if opcion == '1':
+            print('Usted eligió información de la cuenta.')
+            self.cuenta.informacion()
+            return True
+        elif opcion == "2":
+            print("Usted eligió hacer un deposito, ingrese la cantidad a depositar:")
+            cantidad = float(input())
+            self.cuenta.depositar(cantidad)
+            print("   Su saldo es de: ", self.cuenta.saldo, "$")
+            return True
+        elif opcion == "3":
+            print('Usted eligió hacer un retiro, ingrese la cantidad a retirar:')
+            cantidad = float(input())
+            self.cuenta.retirar(cantidad)
+            print("Su saldo es de: ", self.cuenta.saldo, "$")
+            return True
+        elif opcion == "4":
+            print("Usted eligió terminar la sesion, vuelva pronto.")
+            return False
+        else:
+            print('Opcion no valida, elija entre las opciones 1, 2, 3 o 4.')
+            return True
