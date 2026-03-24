8c7100008c1fd91c3d3a59da84f1d69e153c89f0
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-03-24 02:11:25 -0600

    str
    Implementacion de str en cuenta y cliente para posteriormente cambiarlos en menu

--- alskdfklasd.py
+++
@@ -1,6 +0,0 @@
-class Main: 
-    pass
-    cliente1 = ("Angel", "angelismaelaivs@ciencias.unam.mx", 22)
-
-
-    print(str(cliente1)) #sí  funcionó :)
--- teoria/Menu.py
+++ teoria/Menu.py
@@ -1,44 +1,45 @@
-from Cuenta import Cuenta 
+from Cuenta import *
 
 class Menu:
-  def __init__(self, mensaje, cuenta):
-      self.mensajeBienvenida = mensaje
-      self.cuenta = cuenta
 
-  def darBienvenida(self):
-      print("Hola bienvenido a la interfaz de tu cuenta Banco Ciencias.")
+    def __init__(self, mensaje, cuenta):
+        self.mensajeBienvenida = mensaje
+        self.cuenta1 = Cuenta(1000.12, 'Cuenta de ahorro', '1/Enero/2010')
 
-  def despliegaMenu(self):
-        print("------------------------------------------------")
-        print ('Selecciona una opción para continuar:')
-        print('1. Información de cuenta')
-        print('2. Depositar')
-        print('3. Retirar')
-        print('4. Salir')
-        print("------------------------------------------------")
-        
-        
+    def darBienvenida(self):
+        print("Hola bienvenido a la interfaz de tu cuenta Banco Ciencias.")
 
-  def procesaOpcion(self, opcion):
-        if opcion == '1':
-            print('Usted eligió información de la cuenta.')
-            self.cuenta.informacion()
-            return True
-        elif opcion == "2":
-            print("Usted eligió hacer un deposito, ingrese la cantidad a depositar:")
-            cantidad = float(input())
-            self.cuenta.depositar(cantidad)
-            print("   Su saldo es de: ", self.cuenta.saldo, "$")
-            return True
-        elif opcion == "3":
-            print('Usted eligió hacer un retiro, ingrese la cantidad a retirar:')
-            cantidad = float(input())
-            self.cuenta.retirar(cantidad)
-            print("Su saldo es de: ", self.cuenta.saldo, "$")
-            return True
-        elif opcion == "4":
-            print("Usted eligió terminar la sesion, vuelva pronto.")
-            return False
-        else:
-            print('Opcion no valida, elija entre las opciones 1, 2, 3 o 4.')
-            return True
+    def despliegaMenu(self):
+            print("------------------------------------------------")
+            print ('Selecciona una opción para continuar:')
+            print('1. Información de cuenta')
+            print('2. Depositar')
+            print('3. Retirar')
+            print('4. Salir')
+            print("------------------------------------------------")
+            
+            
+
+    def procesaOpcion(self, opcion):
+            if opcion == '1':
+                print('Usted eligió información de la cuenta.')
+                print(self.cuenta1)  #Aquí ocupo el str en Cuenta
+                return True
+            elif opcion == "2":
+                print("Usted eligió hacer un deposito, ingrese la cantidad a depositar:")
+                cantidad = float(input())
+                self.cuenta.depositar(cantidad)
+                print(f"Deposito exitoso, su saldo es de: {self.cuenta1.saldo}")  #Aquí ocupo el str en Cuenta
+                return True
+            elif opcion == "3":
+                print('Usted eligió hacer un retiro, ingrese la cantidad a retirar:')
+                cantidad = float(input())
+                self.cuenta.retirar(cantidad)
+                print(f"Retiro exitoso, su saldo es de: {self.cuenta1.saldo}")  #Aquí ocupo el str en Cuenta
+                return True
+            elif opcion == "4":
+                print("Usted eligió finalizar la sesion, vuelva pronto.")
+                return False
+            else:
+                print('Opcion no valida, elija entre las opciones 1, 2, 3 o 4.')
+                return True
