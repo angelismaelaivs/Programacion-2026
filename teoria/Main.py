e5bb3acc336a5e6533a6df128a8d3930f4bef257
Angel Ismael <angelismaelaivs@ciencias.unam.mx>
2026-03-24 03:31:00 -0600

    Update Main.py

--- teoria/Main.py
+++ teoria/Main.py
@@ -4,13 +4,15 @@ from Cliente import *
 
 class Main: 
     pass
-    menu = Menu("BANCO CIENCIAS")
+    cuenta1 = Cuenta(1000.12, 'Cuenta de ahorro', '1/Enero/2010')
+    cliente1 = Cliente("Angel", "angelismaelaivs@ciencias.unam.mx", 22, cuenta1)  #Aqui relaciono Cliente con Cuenta
+
+    menu = Menu("BANCO CIENCIAS", cuenta1)
     menu.darBienvenida()
 
     opcion = menu.despliegaMenu()
     menu.procesaOpcion(opcion)
 
 
-cuenta1 = Cuenta(1000.12, 'Cuenta de ahorro', '1/Enero/2010')
-cliente1 = Cliente("Angel", "angelismaelaivs@ciencias.unam.mx", 22, cuenta1)  #Aqui relaciono Cliente con Cuenta
+
 
